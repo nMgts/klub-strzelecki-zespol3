20bcf63febec6fadf85ae44196eb9d32d5c7deb3
Norbert Klessen <norbet.klessen@gmail.com>
2024-03-26 21:27:02 +0100

    Dodanie UserService

--- serwer_klub_strzelecki/src/main/java/pl/klubstrzelecki/serwer_klub_strzelecki/service/UserServiceImplementation.java
+++ serwer_klub_strzelecki/src/main/java/pl/klubstrzelecki/serwer_klub_strzelecki/service/UserServiceImplementation.java
@@ -1,2 +1,25 @@
-package pl.klubstrzelecki.serwer_klub_strzelecki.service;public class UserServiceImplementation {
+package pl.klubstrzelecki.serwer_klub_strzelecki.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.klubstrzelecki.serwer_klub_strzelecki.model.User;
+import pl.klubstrzelecki.serwer_klub_strzelecki.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImplementation implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findUserById(long userId) throws Exception {
+        Optional<User> opt = userRepository.findById(userId);
+
+        if (opt.isPresent()) {
+            return opt.get();
+        }
+        throw new Exception("User not found with id" + userId);
+    }
 }
