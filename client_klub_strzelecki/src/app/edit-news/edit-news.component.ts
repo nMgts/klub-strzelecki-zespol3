1c18bcc00d5024a3ad56652f43890a802f2305c6
Norbert Klessen <norbet.klessen@gmail.com>
2024-05-06 10:06:41 +0200

    Edycja newsów 0.2

--- client_klub_strzelecki/src/app/edit-news/edit-news.component.ts
+++ client_klub_strzelecki/src/app/edit-news/edit-news.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {News} from "../interfaces/news";
+import {NewsService} from "../services/news.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-edit-news',
   templateUrl: './edit-news.component.html',
   styleUrl: './edit-news.component.css'
 })
-export class EditNewsComponent {
+export class EditNewsComponent implements OnInit {
+  new_news: News = {
+    title: '',
+    content: ''
+  };
+
+  constructor(
+    private newsService: NewsService,
+    private router: Router) {
+  }
+
+  onSubmit() {
+
+  }
+
+  ngOnInit(): void {
+  }
 
 }
