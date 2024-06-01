ea5a64b71b6554a990cefa086aeda08beb23adc0
Adrian Woźniak <adi23061975@gmail.com>
2024-06-01 17:22:19 +0200

    Footer poprawka i rola w użytkownikach w tabeli

--- client_klub_strzelecki/src/app/app.routes.ts
+++ client_klub_strzelecki/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { ShooterFormComponent } from './shooter-form/shooter-form.component';
 
 export const routes: Routes = [
   {
     path: 'shooters/:id',
-    component: ShooterFormComponent
   }
 ];
