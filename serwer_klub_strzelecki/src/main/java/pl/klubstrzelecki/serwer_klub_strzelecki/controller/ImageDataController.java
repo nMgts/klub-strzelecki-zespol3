c397f2f0de3ba49e3fbdad97252631f04241d750
Eryk Azierski <30671@akademiakaliska.edu.pl>
2024-06-01 19:32:32 +0200

    imagedata

---
+++ serwer_klub_strzelecki/src/main/java/pl/klubstrzelecki/serwer_klub_strzelecki/controller/ImageDataController.java
@@ -0,0 +1,39 @@
+package pl.klubstrzelecki.serwer_klub_strzelecki.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import pl.klubstrzelecki.serwer_klub_strzelecki.service.ImageDataService;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/images")
+public class ImageDataController {
+
+    private ImageDataService imageDataService;
+
+    @Autowired
+    public ImageDataController(ImageDataService imageDataService) {
+        this.imageDataService = imageDataService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
+        String uploadImage = imageDataService.uploadImage(file);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(uploadImage);
+    }
+
+    @GetMapping("/{fileName}")
+    public ResponseEntity<?> downloadImage(@PathVariable String fileName){
+        byte[] imageData=imageDataService.downloadImage(fileName);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png"))
+                .body(imageData);
+
+    }
+}
