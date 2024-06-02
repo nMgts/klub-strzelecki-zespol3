ad60b7c235b3bff85a78c9915ec393e7eefa9623
Norbert Klessen <norbet.klessen@gmail.com>
2024-06-02 03:53:35 +0200

    zaimplementowanie guarda do routingu

--- client_klub_strzelecki/src/app/app-routing.module.ts
+++ client_klub_strzelecki/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { NewsComponent } from './news-list/news.component';
 import { ShootersComponent } from './shooters/shooters.component';
 import { UsersComponent } from './users-list/users.component';
-
 import { AddShooterComponent } from './shooters-add/add-shooters.component';
 import { AddNewsComponent } from './add-news/add-news.component';
 import { EditShooterComponent } from './shooters-edit/edit-shooters.component';
@@ -16,22 +15,23 @@ import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import {UpdateProfileComponent} from "./update-profile/update-profile.component";
 import {AssignShootersComponent} from "./assign-shooters/assign-shooters.component";
+import { usersGuard, adminGuard } from "./guard/users.guard";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'news', pathMatch: 'full' },
   { path: 'news', component: NewsComponent },
-  { path: 'news/add', component: AddNewsComponent },
-  { path: 'news/edit/:id', component: EditNewsComponent },
-  { path: 'shooters', component: ShootersComponent},
-  { path: 'shooters/add', component: AddShooterComponent },
-  { path: 'shooters/edit/:id', component: EditShooterComponent },
-  { path: 'users', component: UsersComponent},
-  { path: 'competitions', component: CompetitionsListComponent},
-  { path: 'competitions/add', component: CompetitionAddComponent},
-  { path: 'competitions/assign/:id', component: AssignShootersComponent },
+  { path: 'news/add', component: AddNewsComponent, canActivate: [adminGuard] },
+  { path: 'news/edit/:id', component: EditNewsComponent, canActivate: [adminGuard] },
+  { path: 'shooters', component: ShootersComponent, canActivate: [adminGuard]},
+  { path: 'shooters/add', component: AddShooterComponent, canActivate: [adminGuard] },
+  { path: 'shooters/edit/:id', component: EditShooterComponent, canActivate: [adminGuard] },
+  { path: 'users', component: UsersComponent, canActivate: [adminGuard]},
+  { path: 'competitions', component: CompetitionsListComponent, canActivate: [usersGuard]},
+  { path: 'competitions/add', component: CompetitionAddComponent, canActivate: [adminGuard]},
+  { path: 'competitions/assign/:id', component: AssignShootersComponent, canActivate: [adminGuard] },
   { path: 'statute', component: StatuteComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'profile/update/:id', component: UpdateProfileComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [usersGuard] },
+  { path: 'profile/update/:id', component: UpdateProfileComponent, canActivate: [usersGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
