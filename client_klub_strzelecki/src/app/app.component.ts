a4b556974abefcf5e07791ebc269efedc06ae72f
arekfr <164903666+arekfr@users.noreply.github.com>
2024-10-22 16:14:45 +0200

    wysuwany navbar

--- client_klub_strzelecki/src/app/app.component.ts
+++ client_klub_strzelecki/src/app/app.component.ts
@@ -1,33 +1,53 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, HostListener} from '@angular/core';
 import {UsersService} from "./services/users.service";
 
 @Component({
   selector: 'app-root',
-
   templateUrl: './app.component.html',
-
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-
+  title = 'Shooting Club';
+  isAuthenticated: boolean = false;
+  isAdmin: boolean = false;
+  isUser: boolean = false;
+  navbarVisible: boolean = false;  // Zmienna do kontrolowania widoczności navbaru
+  isSmallScreen: boolean = false;  // Dodana zmienna do sprawdzania wielkości ekranu
 
   constructor(private readonly userService: UsersService) {}
 
-  title = 'Shooting Club';
-  isAuthenticated:boolean = false;
-  isAdmin:boolean = false;
-  isUser:boolean = false;
-
-  logout():void {
-    this.userService.logOut();
-    this.isAuthenticated = false;
-    this.isAdmin = false;
-    this.isUser = false;
+  // Słuchamy zmian rozmiaru okna
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.checkScreenWidth();  // Sprawdzamy wielkość ekranu przy zmianie rozmiaru
   }
 
   ngOnInit(): void {
     this.isAuthenticated = this.userService.isAuthenticated();
     this.isAdmin = this.userService.isAdmin();
     this.isUser = this.userService.isUser();
+    this.checkScreenWidth();  // Sprawdzamy wielkość ekranu przy inicjalizacji
+  }
+
+  logout(): void {
+    this.userService.logOut();
+    this.isAuthenticated = false;
+    this.isAdmin = false;
+    this.isUser = false;
+  }
+
+  // Przełączanie widoczności navbaru
+  toggleNavbar(): void {
+    this.navbarVisible = !this.navbarVisible;
+  }
+
+  // Funkcja sprawdzająca wielkość ekranu
+  private checkScreenWidth(): void {
+    this.isSmallScreen = window.innerWidth < 730;  // Zaktualizowano na mniejsze ekrany
+    if (!this.isSmallScreen) {
+      this.navbarVisible = true;  // Na większych ekranach zawsze pokazujemy navbar
+    } else {
+      this.navbarVisible = false;  // Na mniejszych ekranach domyślnie ukrywamy navbar
+    }
   }
 }
